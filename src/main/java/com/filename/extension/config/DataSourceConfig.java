3a251521ec5ad87cb2eb92c9c70d76d478df32ba
donoun6 <donoun6@naver.com>
2023-02-12 18:23:53 +0900

    AWS

--- src/main/java/com/filename/extension/config/DataSourceConfig.java
+++ src/main/java/com/filename/extension/config/DataSourceConfig.java
@@ -18,9 +18,9 @@ public class DataSourceConfig {
 		//mysql db
 		DataSource ds = new DataSource();
 		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		ds.setUrl("jdbc:mysql://localhost:3306/extension?serverTimezone=Asia/Seoul");
-		ds.setUsername("extension");
-		ds.setPassword("extension");
+		ds.setUrl("jdbc:mysql://3.36.1.185/extension?serverTimezone=Asia/Seoul");
+		ds.setUsername("test");
+		ds.setPassword("testtest");
 		ds.setInitialSize(2);	// 커넥션 풀 초기화시 생성할 초기 커넥션 개수(기본값 10)
 		ds.setMaxActive(10);	// 풀에서 가져올 수 있는 최대 커넥션 개수(기본값 100)
 		ds.setMaxIdle(10);		// 풀에 유지할 수 있는 최대 커넥션 수(기본값은 MaxActive와 동일)
